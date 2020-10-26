e939792107ee02e7af016d867ad11e434414568e
mark <mark.moskalenko@gmail.com>
2020-10-26 16:31:59 +0300

    Init

---
+++ database/migrations/2020_10_26_131016_add_init_tables.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddInitTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table)
+        {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('products', function (Blueprint $table)
+        {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price');
+            $table->decimal('is_published');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('product_categories', function (Blueprint $table)
+        {
+            $table->id();
+            $table->foreignId('product_id');
+            $table->foreignId('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('/products')->group(function ()
+{
+    Route::get('', [ProductController::class, 'index']);
+    Route::post('', [ProductController::class, 'create']);
+    Route::post('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'delete']);
+});
+
